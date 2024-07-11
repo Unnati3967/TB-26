28bbe2761f6b0f08bfc7b9ba7ac6519af252ffa8
Unnati3967 <unnatipande3967@gmail.com>
2024-07-11 17:58:23 +0530

    added method M1

--- Git3/src/com/cjc/Demo.java
+++ Git3/src/com/cjc/Demo.java
@@ -1,6 +1,9 @@
 package com.cjc;
 
 public class Demo {
+	public void m1{
+		System.out.println("M1 method");
+	}
 public static void main(String[] args) {
 	System.out.println("This is main method");
 }
