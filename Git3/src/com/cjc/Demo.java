3fdd316992964634e049daeac89fd700dec47d62
Unnati3967 <unnatipande3967@gmail.com>
2024-12-11 23:48:30 +0530

    Git
    add m3 method

--- Git3/src/com/cjc/Demo.java
+++ Git3/src/com/cjc/Demo.java
@@ -9,9 +9,9 @@ public class Demo {
 	{
 		System.out.println("M2 method");
 	}
-	public void m2()
+	public void m3()
 	{
-		System.out.println("M2 method");
+		System.out.println("M3 method");
 	}
 public static void main(String[] args) {
 	System.out.println("This is main method");
